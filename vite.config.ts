2b87394bb7fcfbe87c381cd58de8dfc695819c37
Evgeniy19021983 <evgeniyd1983@ukr.net>
2025-09-28 13:47:58 +0300

    Fix deploy for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/03-react-movies/",  // обязательно для GitHub Pages
-  build: {
-    sourcemap: true
-  }
+  base: "", // для Vercel всегда пусто!
 });
