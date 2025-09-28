60042e26fff2d9089e4e07f64d9de408116c01cd
Evgeniy19021983 <evgeniyd1983@ukr.net>
2025-09-28 13:44:17 +0300

    Final version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/03-react-movies/",  // обязательно для GitHub Pages
   build: {
-    sourcemap: true,
-  },
+    sourcemap: true
+  }
 });
